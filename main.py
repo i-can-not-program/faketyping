7d0f648e5b90cba8b2453a82a37e8f12d7d75710
Austomus Gemmail <austomus.minecraft@gmail.com>
2021-09-25 12:59:15 +1200

    first working version

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from time import sleep
+import sys
+import argparse
+
+string = ""
+wait_time = ""
+while len(string) == 0:
+    string = input("Enter a string to be typed: ")
+while True:
+    try:
+        wait_time = float(input("How long should it wait between characters: "))
+    except ValueError:
+        print("ValueError occurred! Your input is not a float!")
+        continue
+    else:
+        break
+chars = list(string)
+sleep(wait_time)
+for char in chars:
+    print(char, end="", flush=True)
+    sleep(wait_time)
+print()
