4e79c11c87c161b4e9463c92701e6b46aa1928d9
Austomus Gemmail <austomus.minecraft@gmail.com>
2021-09-25 13:18:00 +1200

    use argparse

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ import argparse
 
 string = ""
 wait_time = ""
-while len(string) == 0:
-    string = input("Enter a string to be typed: ")
-while True:
-    try:
-        wait_time = float(input("How long should it wait between characters: "))
-    except ValueError:
-        print("ValueError occurred! Your input is not a float!")
-        continue
-    else:
-        break
-chars = list(string)
-sleep(wait_time)
+parser = argparse.ArgumentParser()
+parser.add_argument("string", help="The string that will be typed.", type=str)
+parser.add_argument("-n", "--interval", type=float, default=0.2,
+                    help="The interval between typing characters")
+args = parser.parse_args()
+
+# while len(string) == 0:
+#     string = input("Enter a string to be typed: ")
+# while True:
+#     try:
+#         wait_time = float(input("How long should it wait between characters: "))
+#     except ValueError:
+#         print("ValueError occurred! Your input is not a float!")
+#         continue
+#     else:
+#         break
+chars = list(args.string)
+sleep(args.interval)
 for char in chars:
     print(char, end="", flush=True)
-    sleep(wait_time)
+    sleep(args.interval)
 print()
