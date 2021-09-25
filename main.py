d7350cb85424675c77f5708f24e89b0043caf034
Austomus Gemmail <austomus.minecraft@gmail.com>
2021-09-25 14:25:48 +1200

    Break program (i will fix it soon)

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from time import sleep
 import sys
 import argparse
 
-string = ""
-wait_time = ""
 parser = argparse.ArgumentParser()
-parser.add_argument("string", help="The string that will be typed.", type=str)
+# TODO: fix TypeError: 'required' is an invalid argument for positionals
+parser.add_argument("string", type=str, required=False,
+                    help="The string that will be typed.")
 parser.add_argument("-n", "--interval", type=float, default=0.2,
                     help="The interval between typing characters")
 args = parser.parse_args()
+
 
 # while len(string) == 0:
 #     string = input("Enter a string to be typed: ")
@@ -21,9 +22,19 @@ args = parser.parse_args()
 #         continue
 #     else:
 #         break
-chars = list(args.string)
-sleep(args.interval)
-for char in chars:
-    print(char, end="", flush=True)
+def fake_type(string):
+    chars = list(string)
     sleep(args.interval)
-print()
+    for char in chars:
+        print(char, end="", flush=True)
+        sleep(args.interval)
+    print()
+
+
+# Support for being piped into
+if len(args.string) == 0:
+    for line in sys.stdin:
+        fake_type(line.rstrip())
+else:
+    # Type from string argument
+    fake_type(args.string)
