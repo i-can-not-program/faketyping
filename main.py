f0c767dd6ae1fda14ba3a251829a450b2d6294a6
Austomus Gemmail <austomus.minecraft@gmail.com>
2021-09-27 21:21:13 +1300

    Added feature: Random delay added to each keystroke from #2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from time import sleep
 import sys
 import argparse
+import random
 
 # Set up argparse
 parser = argparse.ArgumentParser(description="Python program of which the output looks similar to typing.")
@@ -9,27 +10,40 @@ parser.add_argument("string", type=str, nargs="*",
                     help="The string that will be typed. If this argument is not specified, the input will be read "
                          "from stdin.")
 parser.add_argument("-n", "--interval", type=float, default=0.2,
-                    help="The interval between typing characters")
+                    help="The interval between typing characters.")
+parser.add_argument("-o", "--offset", type=float,
+                    help="The maximum random offset. The minimum is the same, but converted to a negative number.")
 args = parser.parse_args()
+# Error if the interval is negative
+if args.interval < 0:
+    raise ValueError("interval must be a non-negative number")
+# Set offset and give errors
+if args.offset is None:
+    args.offset = args.interval / 2
+elif args.offset > args.interval:
+    raise ValueError("offset must be less than the interval")
+elif args.offset < 0 - args.interval:
+    raise ValueError("offset must be greater than the interval converted to a negative number")
 
 
 # Create fake_type function
 def fake_type(string):
+    # Create a list of all the characters in the string argument
     chars = list(string)
+    # For each character in the list, sleep and print it
     for char in chars:
-        sleep(args.interval)
+        sleep(args.interval + random.uniform(0 - args.offset, args.offset))
         print(char, end="", flush=True)
 
 
-# If the string argument is not specified, read from stdin
-if not args.string:
-    # Type each line in stdin
-    for line in sys.stdin:
-        fake_type(line)
-else:
+# Test if the string argument is specified
+if args.string:
     # Type from string argument
     for word in args.string:
         fake_type(word + " ")
     # Print to add newline at the end
     print()
-
+else:
+    # Type each line in stdin
+    for line in sys.stdin:
+        fake_type(line)
